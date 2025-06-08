364a83a7de9ff00407a0e5891b87dae3af223b73
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-06-07 23:27:29 -0400

    lock-then-hibernate.sh now won't lock if sound is playing; useful for watching long videos.

--- lock-then-hibernate.sh
+++ lock-then-hibernate.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-i3lock -c 000000
-lock_pid=$!
-sleep 180 
-if kill -0 $lock_pid 2>/dev/null; then
-	kill $lock_pid
-	wait $lock_pid 2>/dev/null
-	systemctl hibernate
+# Check if sound is being played:
+if [ -z "$(cat /proc/asound/card*/pcm*/sub*/status | grep RUNNING)" ]; then
+	# Locking mechanism
+	i3lock -c 000000 &
+	lock_pid=$!
+	sleep 180 
+	if kill -0 "$lock_pid" 2>/dev/null; then
+		kill "$lock_pid"
+		wait "$lock_pid" 2>/dev/null
+		systemctl hibernate
+	fi
 fi
