3351d730c2bc48a23ebcec892b873db68aa7b2fc
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-06-08 15:02:50 -0400

    Actually made it work this time...

--- lock-then-hibernate.sh
+++ lock-then-hibernate.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+sleep_interval=180 # Seconds
+
 # Check if sound is being played:
-if [ -z "$(cat /proc/asound/card*/pcm*/sub*/status | grep RUNNING)" ]; then
+check_sound_status()
+{
+	pactl list sinks | grep "RUNNING"
+	return $?
+	echo "ERR: Cannot read sound device status" >&2
+	return 1
+}
+
+if ! check_sound_status; then
 	# Locking mechanism
 	i3lock -c 000000 &
 	lock_pid=$!
-	sleep 180 
+	sleep "$sleep_interval"
+
+	# Cleanup
 	if kill -0 "$lock_pid" 2>/dev/null; then
 		kill "$lock_pid"
 		wait "$lock_pid" 2>/dev/null
