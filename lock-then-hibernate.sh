1d345a9a24a86b0d3365c2c9540a826ad1a6c993
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-06-08 17:48:36 -0400

    Turns out it doesn't work. I'm reverting it to just a regular lock -- no sound checking.

--- lock-then-hibernate.sh
+++ lock-then-hibernate.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 
-sleep_interval=180 # Seconds
+# Locking mechanism
+i3lock -c 000000 &
+lock_pid=$!
+sleep "$sleep_interval"
 
-# Check if sound is being played:
-check_sound_status()
-{
-	pactl list sinks | grep "RUNNING"
-	return $?
-	echo "ERR: Cannot read sound device status" >&2
-	return 1
-}
-
-if ! check_sound_status; then
-	# Locking mechanism
-	i3lock -c 000000 &
-	lock_pid=$!
-	sleep "$sleep_interval"
-
-	# Cleanup
-	if kill -0 "$lock_pid" 2>/dev/null; then
-		kill "$lock_pid"
-		wait "$lock_pid" 2>/dev/null
-		systemctl hibernate
-	fi
+# Cleanup
+if kill -0 "$lock_pid" 2>/dev/null; then
+	kill "$lock_pid"
+	wait "$lock_pid" 2>/dev/null
+	systemctl hibernate
 fi
